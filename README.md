25a3eefa307a1bf775aaa3a6debc610978546bb3
Allan Feid <afeid@shutterstock.com>
2014-02-19 12:52:51 -0500

    Support the new children API in optopus.
    The cli now can return children information stored in optopus.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Optopus CLI
-This uses the optopus search API to get information about nodes as well as execute commands against the search results.
+This uses the optopus API to get information about nodes as well as execute commands against the search results.
 
 ### Basic configuration
 To get going, you can set the environment variable <code>OPTOPUS_ENDPOINT</code> or the flag <code>--optopus-endpoint</code> to the location of your optopus installation.
@@ -7,7 +7,7 @@ To get going, you can set the environment variable <code>OPTOPUS_ENDPOINT</code>
 ### Example queries
 Search queries are passed to optopus which in turn just uses elasticsearch. Pretty much any elasticsearch query string will work. The <code>--show-facts</code> flag can be used to display facts about the resulting nodes.
 
-    $ optopus-cli location:nyc02 and facts.productname:PowerEdge --show-facts productname ipaddress
+    $ optopus-cli search location:nyc02 and facts.productname:PowerEdge --show-facts productname ipaddress
     dev-db01.nyc02.foo.net
         productname: PowerEdge R510
           ipaddress: 10.0.88.25
@@ -21,11 +21,20 @@ Search queries are passed to optopus which in turn just uses elasticsearch. Pret
 ### Run commands against the resulting hosts
 Running commands just uses Fabric. You can set the login user by setting the environment variable <code>FAB_USER</code>, or passing in the <code>--fab-user</code> flag. If you want to run comands in parallel, use the <code>--parallel</code> flag.
 
-    $ optopus-cli location:tx01 and facts.productname:PowerEdge --run uptime
+    $ optopus-cli search location:tx01 and facts.productname:PowerEdge --run uptime
 
 Or with sudo
 
-    $ optopus-cli location:ma01 and facts.productname:PowerEdge --sudo tail /var/log/messages
+    $ optopus-cli search location:ma01 and facts.productname:PowerEdge --sudo tail /var/log/messages
 
-### Additional functionality
+#### Additional functionality
 You can use the <code>--put</code> flag to scp files to remote hosts and the <code>--get</code> flag to retrieve files from remote hosts.
+
+### Getting child nodes
+This feature will return all children that are related to the supplied node.
+
+
+    $ optopus-cli get-children some-switch
+    some-host1
+    some-host2
+    ...snip...
--- optopus.py
+++ optopus.py
@@ -26,7 +26,11 @@ class Client(object):
         path = "/api/search?string=%s" % urllib2.quote(string)
         if types:
             path += "&types=%s" % ','.join(types)
-        return self._get(path)
+        return self._get(path)['results']
+
+    def get_children(self, hostname):
+        path = "/api/node/%s/children" % hostname
+        return self._get(path)['children']
 
     def _get(self, path):
         req = urllib2.Request("%s%s" % (self._endpoint, path))
@@ -36,24 +40,35 @@ class Client(object):
             results = req.get_full_url()
         else:
             url = urllib2.urlopen(req)
-            results = json.loads(url.read())['results']
+            results = json.loads(url.read())
         return results
 
 class CLI(object):
     @classmethod
     def run(class_):
         class_.parse_args()
-        client = Client(class_.args.optopus_endpoint, class_.args.show_url)
+        class_.client = Client(class_.args.optopus_endpoint, class_.args.show_url)
+        sys.exit(class_.args.func(class_.args))
+
+    @classmethod
+    def search(class_, args):
         types = ['node', 'hypervisor', 'network_node']
-        query_string = ' '.join(class_.args.query)
-        results = client.search(query_string, types)
-        if class_.args.show_url:
+        query_string = ' '.join(args.query)
+        results = class_.client.search(query_string, types)
+        if args.show_url:
             print results
             sys.exit(0)
         if class_.check_args_for_fabric():
             class_.execute_fabric(class_.get_hosts(results))
         else:
             class_.display_hosts(results)
+        return 0
+
+    @classmethod
+    def get_children(class_, args):
+        for child in class_.client.get_children(args.hostname):
+            print child
+        return 0
 
     @classmethod
     def execute_fabric(class_, hosts):
@@ -109,17 +124,27 @@ class CLI(object):
 
     @classmethod
     def parse_args(class_):
+        parsers = {}
         parser = ArgumentParser(description='Search the optopus api and perform various actions')
-        parser.add_argument('query', metavar='QUERY', nargs='+', help="Query for nodes, this can take any elasticsearch parameters compatble with a search string")
-        parser.add_argument('--show-url', action='store_true', default=False, help="Return the optopus url that will be called and exit")
-        parser.add_argument('-sF', '--show-facts', nargs='+', metavar='FACT', help="Show facts about the resulting nodes")
-        parser.add_argument('-sP', '--show-properties', nargs='+', metavar='PROPERTY', help="Show properties about the resulting nodes")
         parser.add_argument('-e', '--optopus-endpoint', default=os.environ.get('OPTOPUS_ENDPOINT', None))
-        parser.add_argument('--run', metavar='SHELL', nargs='+', help="Run a shell command against the resulting hosts")
-        parser.add_argument('--sudo', metavar='SHELL', nargs='+', help="Run a shell command using sudo against the resulting hosts")
-        parser.add_argument('--get', metavar='REMOTE_PATH', help="Pull down remote files from resulting hosts")
-        parser.add_argument('--put', metavar='LOCAL_PATH REMOTE_PATH', nargs=2, help="Upload LOCAL_PATH to REMOTE_PATH")
-        parser.add_argument('--fab-user', metavar='USER', default=os.environ.get('FAB_USER', os.environ.get('USER')), help="When invoking Fabric, use this user to run shell commands")
-        parser.add_argument('-p', '--parallel', action='store_true', default=False, help="Run ssh commands in parallel")
+        parser.add_argument('--show-url', action='store_true', default=False, help="Return the optopus url that will be called and exit")
+        subparsers = parser.add_subparsers(title='subcommands')
+
+        parsers['search'] = subparsers.add_parser('search')
+        parsers['search'].set_defaults(func=class_.search)
+        parsers['search'].add_argument('query', metavar='QUERY', nargs='+', help="Query for nodes, this can take any elasticsearch parameters compatble with a search string")
+        parsers['search'].add_argument('-sF', '--show-facts', nargs='+', metavar='FACT', help="Show facts about the resulting nodes")
+        parsers['search'].add_argument('-sP', '--show-properties', nargs='+', metavar='PROPERTY', help="Show properties about the resulting nodes")
+        parsers['search'].add_argument('--run', metavar='SHELL', nargs='+', help="Run a shell command against the resulting hosts")
+        parsers['search'].add_argument('--sudo', metavar='SHELL', nargs='+', help="Run a shell command using sudo against the resulting hosts")
+        parsers['search'].add_argument('--get', metavar='REMOTE_PATH', help="Pull down remote files from resulting hosts")
+        parsers['search'].add_argument('--put', metavar='LOCAL_PATH REMOTE_PATH', nargs=2, help="Upload LOCAL_PATH to REMOTE_PATH")
+        parsers['search'].add_argument('--fab-user', metavar='USER', default=os.environ.get('FAB_USER', os.environ.get('USER')), help="When invoking Fabric, use this user to run shell commands")
+        parsers['search'].add_argument('-p', '--parallel', action='store_true', default=False, help="Run ssh commands in parallel")
+
+        parsers['get-children'] = subparsers.add_parser('get-children')
+        parsers['get-children'].add_argument('hostname', metavar='HOSTNAME', help="Show children for the supplied hostname")
+        parsers['get-children'].set_defaults(func=class_.get_children)
+
         class_.args = parser.parse_args()
 
